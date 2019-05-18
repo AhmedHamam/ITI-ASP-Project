d027f64e3d913f7bb2216d8221d2180f19624a6a
Ahmed Hamam <ahmedhamam50@gmail.com>
2019-05-19 01:23:19 +0200

    intial

---
+++ Ecomerce Website/Ecomerce Website/data acess layer/DBClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ecomerce_Website
+{
+    public static class BDlayer
+    {
+        public static DataTable select(SqlCommand query)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["iticon"].ConnectionString);
+            SqlCommand cmd = query;
+            cmd.Connection = con;
+            SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            return dt;
+        }
+
+        public static int DML(SqlCommand command)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["iticon"].ConnectionString);
+            SqlCommand cmd = command;
+            cmd.Connection = con;
+            con.Open();
+            int roweffect = cmd.ExecuteNonQuery();
+            con.Close();
+            return roweffect;
+        }
+    }
+}
---
+++ Ecomerce Website/Ecomerce Website/login.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="Ecomerce_Website.login" %>
+<%@ Register src="UC_login.ascx" tagname="UC_login" tagprefix="uc1" %>
+
+<asp:Content ID="Content1" runat="server" contentplaceholderid="Login_placeHolder">
+    <table style="width: 100%">
+        <tr>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
+
